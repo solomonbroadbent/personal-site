369eab02b9356fe8be1831a257bad60ce987d4ef
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2021-06-24 02:07:30 +1200

    add fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,16 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-	return <main>test</main>;
+	return (
+		<main>
+			<Head>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Reenie+Beanie&Open+Sans'
+					rel='stylesheet'
+				/>
+				<title>sol</title>
+			</Head>
+			<h1>Solomon Broadbent</h1>
+		</main>
+	);
 }
