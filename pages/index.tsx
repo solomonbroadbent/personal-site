c04a5dcdf3ed821d2a29d47f770bf3f1dfb5c04a
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2021-06-25 06:11:58 +1200

    refactor html structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
-		<main>
+		<div id={styles.root}>
 			<Head>
 				<link
 					href='https://fonts.googleapis.com/css2?family=Shadows+Into+Light&Open+Sans'
@@ -16,48 +16,41 @@ export default function Home() {
 				/>
 				<title>sol</title>
 			</Head>
-			<header id={styles['landing-section']}>
+			<nav id={styles['navigation-section']}>
+				<h1
+					className={`${styles.link} ${styles['link--extra-margin-bottom']} hoverable`}
+				>
+					skill set
+				</h1>
+				<h1 className={`${styles.link} ${styles['link--active']}`}>
+					terrain tinker
+				</h1>
+				<h1 className={`${styles.link} hoverable`}>playlistsyn.cr</h1>
+				<h1 className={`${styles.link} hoverable`}>chorus</h1>
+				<h1 className={`${styles.link} hoverable`}>nicholson consulting</h1>
+				<h1
+					className={`${styles.link} ${styles['link--extra-margin-top']} hoverable`}
+				>
+					uni
+				</h1>
+				<h1 className={`${styles.link} hoverable`}>about me</h1>
+			</nav>
+			<footer id={styles.contact}>
 				<h1>Solomon Broadbent</h1>
-				<h2>Full Stack Dev</h2>
-			</header>
-			<section id={styles['navigation-section']}>
-				<div id={styles.links}>
-					<h1
-						className={`${styles.link} ${styles['link--extra-margin-bottom']} hoverable`}
-					>
-						skill set
-					</h1>
-					<h1 className={`${styles.link} ${styles['link--active']}`}>
-						terrain tinker
-					</h1>
-					<h1 className={`${styles.link} hoverable`}>playlistsyn.cr</h1>
-					<h1 className={`${styles.link} hoverable`}>chorus</h1>
-					<h1 className={`${styles.link} hoverable`}>nicholson consulting</h1>
-					<h1
-						className={`${styles.link} ${styles['link--extra-margin-top']} hoverable`}
-					>
-						uni
-					</h1>
-					<h1 className={`${styles.link} hoverable`}>about me</h1>
-				</div>
-				<div id={styles.contact}>
-					<h1>Solomon Broadbent</h1>
-					<h2>full stack dev</h2>
-					<button
-						id={styles['email-button']}
-						className={`${styles.button} hoverable`}
-					>
-						email
-					</button>
-					<button
-						id={styles['blog-button']}
-						className={`${styles.button} hoverable`}
-					>
-						blog
-					</button>
-				</div>
-			</section>
-			<section id={styles['main-section']}></section>
-		</main>
+				<h2>full stack dev</h2>
+				<button
+					id={styles['email-button']}
+					className={`${styles.button} hoverable`}
+				>
+					email
+				</button>
+				<button
+					id={styles['blog-button']}
+					className={`${styles.button} hoverable`}
+				>
+					blog
+				</button>
+			</footer>
+		</div>
 	);
 }
