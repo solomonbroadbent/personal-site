50b88ba969ae1293a651d5bafd7ed54c561b2bf3
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2021-07-01 09:17:15 +1200

    correctly show active nav link on scroll on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Section from '../components/Section';
 import { useEffect, useRef, useState } from 'react';
@@ -34,39 +33,67 @@ export default function Home() {
 	];
 	const [activeSection, setActiveSection] = useState(sections[0]);
 
-	const loadNextSection = () => {};
-	const loadPreviousSection = () => {};
+	const changeActiveSectionOnScroll = (scrollY: number) => {
+		const mainSection = document.getElementById(styles.main);
+		// TODO: add logging
+		if (mainSection === null) return;
 
-	const blahOnScroll = event => {
-		const mainScrollPosition = event.target.scrollTop + event.target.offsetTop;
+		const amountScrolled = scrollY + mainSection.offsetTop;
+
 		sections
 			.filter(section => {
 				const sectionTop = section.ref.current.offsetTop;
 				const sectionBottom = section.ref.current.clientHeight + sectionTop;
 
-				return (
-					mainScrollPosition >= sectionTop &&
-					mainScrollPosition <= sectionBottom
-				);
+				return amountScrolled >= sectionTop && amountScrolled <= sectionBottom;
 			})
 			.forEach(setActiveSection);
 	};
 
+	// this is only for the mobile layout as the "main" section isn't overflowing in this scenario,
+	//	the window is. sadly this means it's a bit more confusing than it should be.
+	useEffect(() => {
+		const changeActiveSectionBasedOnWindowScrollY = () => {
+			changeActiveSectionOnScroll(window.scrollY);
+
+			const nav = document.getElementById(styles.nav);
+			// TODO: add logging
+			if (nav === null) return;
+
+			const activeNavLink = nav
+				.getElementsByClassName(
+					'Home_nav__link--active__1vwwm',
+					// styles.nav__linkActive,
+				)
+				.item(0);
+			// TODO: add logging
+			if (activeNavLink === null) return;
+
+			const halfTheRemainingClientWidth = (nav.clientWidth - activeNavLink.clientWidth) / 2;
+
+			nav.scrollLeft = activeNavLink.offsetLeft - halfTheRemainingClientWidth;
+		};
+
+		// TODO: most probably want to debounce around here
+		window.addEventListener('scroll', changeActiveSectionBasedOnWindowScrollY);
+		// TODO: check this resize event listener is a good idea
+		window.addEventListener('resize', changeActiveSectionBasedOnWindowScrollY);
+
+		return function cleanup() {
+			window.removeEventListener('scroll', changeActiveSectionBasedOnWindowScrollY);
+			window.removeEventListener('resize', changeActiveSectionBasedOnWindowScrollY);
+		};
+	}, []);
+
 	return (
 		<div id={styles.root}>
 			<Head>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Shadows+Into+Light&Open+Sans'
-					rel='stylesheet'
-				/>
-				<meta
-					name='viewport'
-					content='viewport-fit=cover, width=device-width, initial-scale=1, minimum-scale=1'
-				/>
+				<link href='https://fonts.googleapis.com/css2?family=Shadows+Into+Light&Open+Sans' rel='stylesheet' />
+				<meta name='viewport' content='viewport-fit=cover, width=device-width, initial-scale=1, minimum-scale=1' />
 				<title>sol</title>
 			</Head>
 			<Nav sections={sections} activeNavLink={activeSection} />
-			<main id={styles.main} onScroll={blahOnScroll}>
+			<main id={styles.main} onScroll={scrollEvent => changeActiveSectionOnScroll(scrollEvent.target.scrollTop)}>
 				{sections.map(section => {
 					return (
 						<Section key={section.urlName} ref={section.ref}>
