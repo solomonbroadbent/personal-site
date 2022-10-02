351f22465350dce1ffe3119e70bcab410333ff56
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-10-02 20:55:13 +1030

    add optimisations for updated nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+
+	// use much faster minification
+	//	https://nextjs.org/docs/advanced-features/compiler#minification
   swcMinify: true,
+
+	// optimisation for reducing deployment size...
+	//	https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
+	output: 'standalone',
 }
